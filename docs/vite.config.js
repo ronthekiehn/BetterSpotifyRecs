3a0aee29a2c3a13b6f0d6011db4b88e99afc9d23
Ron Kiehn <ronthekiehn@gmail.com>
2024-07-29 18:05:38 -0500

    Update vite.config.js

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -5,5 +5,13 @@ export default defineConfig({
     __SERVER_IP__: JSON.stringify(process.env.SERVER_IP),
     __CLIENT_ID__: JSON.stringify(process.env.CLIENT_ID),
     __CLIENT_SECRET__: JSON.stringify(process.env.CLIENT_SECRET)
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'callback.html'),
+      },
+    },
   }
 });
