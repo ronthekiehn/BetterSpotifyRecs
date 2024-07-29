9485b54e7ae3f66501be0e8d5bf788ff99acd914
Ron Kiehn <ronthekiehn@gmail.com>
2024-07-29 13:57:17 -0500

    what!

--- server/server.js
+++ server/server.js
@@ -166,7 +166,7 @@ app.get('/read-data/:purpose', (req, res) => {
   });
   
 
-app.listen(port, () => {
+app.listen(port, 0.0.0.0, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
