f292e7aff482cd000004a36c2d06ab769f74ab8d
Ron Kiehn <ronthekiehn@gmail.com>
2024-07-30 11:30:57 -0500

    vite is back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   define: {
     __SERVER_IP__: JSON.stringify(process.env.SERVER_IP),
     __CLIENT_ID__: JSON.stringify(process.env.CLIENT_ID),
-    __CLIENT_SECRET__: JSON.stringify(process.env.CLIENT_SECRET)
+    __CLIENT_SECRET__: JSON.stringify(process.env.CLIENT_SECRET),
     __API_URL__ = JSON.stringify(process.env.API_URL);
   },
   build: {
