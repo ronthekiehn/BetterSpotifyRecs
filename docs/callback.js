b92e5abab8b84c7f7e3f3b77be58d5f804b4fb16
Ron Kiehn <ronthekiehn@gmail.com>
2024-07-29 19:28:22 -0500

    git push

--- docs/callback.js
+++ docs/callback.js
@@ -32,6 +32,8 @@ async function handleCallback() {
     const code = params.get('code');
     const state = params.get('state');
     var storedState = document.cookies ? document.cookies[stateKey] : null;
+    console.log(document.cookies);
+    console.log(document.cookies[stateKey]);
     console.log(state);
     console.log(storedState);
     if (!state || state !== storedState) {
