6efaa3c27eee1a76f4f74df34fbf46575310d61b
Ron Kiehn <ronthekiehn@gmail.com>
2024-08-07 15:17:27 -0500

    more cors!!

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://better-spotify-recs.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        return res.status(200).json({});
+    }
+    next();
+});
+
 
 app.use(express.json());
 
