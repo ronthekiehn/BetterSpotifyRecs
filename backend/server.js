832fae9131efef08364674147b95847484a28350
Ron Kiehn <ronthekiehn@gmail.com>
2024-08-07 15:26:41 -0500

    preflight requests buddy

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// app.use(cors({
-//   origin: 'https://better-spotify-recs.vercel.app',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors({
+  origin: 'https://better-spotify-recs.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  preflightContinue: false,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://better-spotify-recs.vercel.app");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === 'OPTIONS') {
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        return res.status(200).json({});
-    }
-    next();
-});
 
 
 app.use(express.json());
