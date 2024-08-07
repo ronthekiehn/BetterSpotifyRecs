3c4b85bf19094ebe668260bb96371b2edd92b014
Ron Kiehn <ronthekiehn@gmail.com>
2024-08-07 17:43:15 -0500

    import errors

--- backend/api/post.js
+++ backend/api/post.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { init, startPlaying, switchDevice, nextTrack, previousTrack, likeTrack, unlikeTrack,fetchWebApi } = require('../cron/cron');
-const {fetchWebApi} = require('../utils/spotify');
+const { init, startPlaying, switchDevice, nextTrack, previousTrack, likeTrack, unlikeTrack } = require('../cron/cron');
+const { fetchWebApi } = require('../utils/spotify');
 
 router.post('/', async (req, res) => {
     const { action, token, playerID, accountName} = req.query;
