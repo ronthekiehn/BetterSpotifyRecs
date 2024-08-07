d9982751a606c3c5759298ca0597fb540a363c96
Ron Kiehn <ronthekiehn@gmail.com>
2024-08-07 16:49:22 -0500

    500 500 500

--- backend/utils/spotify.js
+++ backend/utils/spotify.js
@@ -1,4 +1,5 @@
 async function fetchWebApi(endpoint, method, token, body) {
+    console.log("fetching", endpoint, method, token, body);
     const res = await fetch(`https://api.spotify.com/${endpoint}`, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -7,6 +8,7 @@ async function fetchWebApi(endpoint, method, token, body) {
         
         body:JSON.stringify(body)
       });
+    console.log("response", res);
     if (method = 'GET') {
           return await res.json();
     } 
