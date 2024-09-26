30ea0d40f5256aea974734434b9d25ec52df5809
Ron Kiehn <ronthekiehn@gmail.com>
2024-09-25 20:16:17 -0500

    vercel never built it bruh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        callback: resolve(__dirname, 'callback.html'),
       },
     },
   }
